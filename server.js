ee597606626c4ca8f8f8d389f4142561c1807622
Jakub <jakub.harasny@fuseuniversal.com>
2018-11-17 01:02:50 +0000

    setup start script and heroku port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const hbs = require('hbs'); // eslint-disable-line
 const app = express();
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(`${__dirname}/views/partials`);
 app.set('view engine', 'hbs');
 
@@ -54,6 +56,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on localhost:3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
